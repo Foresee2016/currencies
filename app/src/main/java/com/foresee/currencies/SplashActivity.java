03226c57d8879fb8a1d27ea8610ad14b5d67fb6d
Foresee2016 <1123520670@qq.com>
2018-05-13 20:21:45 +0800

    Fetches codes as json from openexchangerates.org and passes ArrayList to MainActivity with Intent

--- app/src/main/java/com/foresee/currencies/SplashActivity.java
+++ app/src/main/java/com/foresee/currencies/SplashActivity.java
@@ -1,21 +1,61 @@
 package com.foresee.currencies;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Window;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Iterator;
 
 public class SplashActivity extends Activity {
+    public static final String URL_CODES = "http://openexchangerates.org/api/currencies.json";
+    public static final String KEY_ARRAYLIST = "key_arraylist";
+    private ArrayList<String> mCurrencies;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_splash);
+        new FetchCodesTask().execute(URL_CODES);
     }
 
+    private class FetchCodesTask extends AsyncTask<String, Void, JSONObject> {
+
+        @Override
+        protected JSONObject doInBackground(String... strings) {
+            return new JSONParser().getJSONFromUrl(strings[0]);
+        }
+
+        @Override
+        protected void onPostExecute(JSONObject jsonObject) {
+            try {
+                if (jsonObject == null) {
+                    throw new JSONException("no data available");
+                }
+                Iterator<String> iterator = jsonObject.keys();
+                String key = "";
+                mCurrencies = new ArrayList<>();
+                while (iterator.hasNext()) {
+                    key = iterator.next();
+                    mCurrencies.add(key + "|" + jsonObject.getString(key));
+                }
+                Intent mainIntent=new Intent(SplashActivity.this, MainActivity.class);
+                mainIntent.putExtra(KEY_ARRAYLIST, mCurrencies);
+                startActivity(mainIntent);
+                finish();
+            } catch (JSONException e) {
+                Toast.makeText(SplashActivity.this, "There is a JSON exception: "
+                        + e.getMessage(), Toast.LENGTH_LONG).show();
+                e.printStackTrace();
+                finish();
+            }
+        }
+    }
 }
